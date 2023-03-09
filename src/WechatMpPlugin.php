f652789ff8450c2816d3b42990f60b8b8ff2f4b7
twinh <twinhuang@qq.com>
2023-03-09 22:00:20 +0800

    feat: 菜单增加权限配置

--- src/WechatMpPlugin.php
+++ src/WechatMpPlugin.php
@@ -3,6 +3,7 @@
 namespace Miaoxing\WechatMp;
 
 use Miaoxing\Admin\Service\AdminMenu;
+use Miaoxing\App\Service\PermissionMap;
 use Miaoxing\Plugin\BasePlugin;
 use Miaoxing\Wxa\Payment\WxaPay;
 
@@ -20,6 +21,16 @@ class WechatMpPlugin extends BasePlugin
         $menu->child('setting')->addChild()->setLabel('小程序设置')->setUrl('admin/wechat-mp/account');
 
         $menu->child('user')->addChild()->setLabel('小程序用户管理')->setUrl('admin/wechat-mp/users')->setSort(900);
+    }
+
+    public function onPermissionGetMap(PermissionMap $map)
+    {
+        $map->prefix('admin/wechat-mp', function (PermissionMap $map) {
+            $map->add('account', [
+                'PATCH api/admin/wechat-mp/account'
+            ]);
+            $map->addList('users');
+        });
     }
 
     public function onPaymentGetTypes(&$types)
