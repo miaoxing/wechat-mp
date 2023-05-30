6d38417f29e21d75bbadb0d49bd9ee6784bc980d
twinh <twinhuang@qq.com>
2023-05-30 14:05:23 +0800

    feat(wechat-mp): 增加接口返回用户是否已经授权过

---
+++ pages/m-api/wechat-mp/auth/index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Miaoxing\Plugin\BaseController;
+use Miaoxing\Plugin\Service\User;
+
+return new class () extends BaseController {
+    public function get()
+    {
+        $user = User::cur();
+
+        $isAuthed = (bool) $user->mobileVerifiedAt;
+
+        return suc([
+            'isAuthed' => $isAuthed,
+        ]);
+    }
+};
