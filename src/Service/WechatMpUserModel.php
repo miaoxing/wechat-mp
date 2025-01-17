964a20a2029bddd5c4853fd377eb7b34f696474f
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/WechatMpUserModel.php
+++ src/Service/WechatMpUserModel.php
@@ -9,10 +9,28 @@ use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
 use Miaoxing\User\Service\UserModel;
-use Miaoxing\WechatMp\Metadata\WechatMpUserTrait;
 
 /**
  * @property UserModel $user
+ * @property string|null $id
+ * @property string $appId 应用编号
+ * @property string $userId 用户编号
+ * @property string $openId 微信用户 OpenID
+ * @property string $unionId 微信用户 UnionID
+ * @property string $nickName 昵称
+ * @property string $avatarUrl 头像地址
+ * @property string $origAvatarUrl 原始微信头像地址
+ * @property int $gender 性别。0:未知;1:男;2:女
+ * @property string $country 国家
+ * @property string $province 城市
+ * @property string $city 省份
+ * @property string $language 语言
+ * @property string $sessionKey
+ * @property string|null $updatedInfoAt 最后更新信息时间
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
  */
 class WechatMpUserModel extends BaseModel
 {
@@ -21,7 +39,6 @@ class WechatMpUserModel extends BaseModel
     use ModelTrait;
     use ReqQueryTrait;
     use SnowflakeTrait;
-    use WechatMpUserTrait;
 
     public function user(): UserModel
     {
